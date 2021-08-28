323178928e89354e914fb7d630161bb143831ee0
Kalka <k@kalka.io>
2021-08-28 17:05:42 -0400

    Add fade animation to overlay progress bar

--- src/main/java/snownee/jade/client/ClientHandler.java
+++ src/main/java/snownee/jade/client/ClientHandler.java
@@ -1,6 +1,6 @@
 package snownee.jade.client;
 
-import java.awt.Rectangle;
+import java.awt.*;
 
 import mcp.mobius.waila.api.RenderContext;
 import mcp.mobius.waila.api.event.WailaRenderEvent;
@@ -20,6 +20,9 @@ import snownee.jade.JadePlugin;
 @OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(Dist.CLIENT)
 public final class ClientHandler {
+	private static float savedProgress;
+	private static int progressAlpha;
+	private static long fadeTime;
 
 	@SubscribeEvent
 	public static void post(WailaRenderEvent.Post event) {
@@ -28,17 +31,43 @@ public final class ClientHandler {
 		}
 		Minecraft mc = Minecraft.getInstance();
 		PlayerController playerController = mc.playerController;
-		if (playerController == null || !playerController.getIsHittingBlock()) {
+		if (playerController == null) {
 			return;
 		}
-		BlockState state = mc.world.getBlockState(playerController.currentBlock);
-		boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
-		int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
-		Rectangle rect = event.getPosition();
-		float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
-		progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
-		progress = MathHelper.clamp(progress, 0, 1);
-		AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * progress), rect.y + rect.height + 1, color);
+		if (playerController.getIsHittingBlock()) {
+			BlockState state = mc.world.getBlockState(playerController.currentBlock);
+			boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
+			int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
+			if (System.currentTimeMillis() > fadeTime) {
+				if (progressAlpha < 200) {
+					fadeTime = System.currentTimeMillis() + 10;
+					progressAlpha = progressAlpha + 5;
+				}
+			}
+			Color fadeColor = new Color(color);
+			Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), progressAlpha);
+			System.out.println(progressAlpha);
+			Rectangle rect = event.getPosition();
+			float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
+			progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
+			progress = MathHelper.clamp(progress, 0, 1);
+			AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * progress), rect.y + rect.height + 1, alphaColor.getRGB());
+			savedProgress = progress;
+		} else {
+			if (progressAlpha > 0) {
+				Rectangle rect = event.getPosition();
+				BlockState state = mc.world.getBlockState(playerController.currentBlock);
+				boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
+				int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
+				Color fadeColor = new Color(color);
+				Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), progressAlpha);
+				AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * savedProgress), rect.y + rect.height + 1, alphaColor.getRGB());
+				if (System.currentTimeMillis() > fadeTime) {
+					fadeTime = System.currentTimeMillis() + 10;
+					progressAlpha = progressAlpha - 5;
+				}
+			}
+		}
 	}
 
 }
