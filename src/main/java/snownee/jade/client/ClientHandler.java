fb82bd11e72da140d3b68063a117bac966e871eb
Kalka <k@kalka.io>
2021-08-28 17:37:29 -0400

    clean up duplicates

--- src/main/java/snownee/jade/client/ClientHandler.java
+++ src/main/java/snownee/jade/client/ClientHandler.java
@@ -31,41 +31,36 @@ public final class ClientHandler {
 		}
 		Minecraft mc = Minecraft.getInstance();
 		PlayerController playerController = mc.playerController;
-		if (playerController == null) {
+		if (playerController == null || playerController.currentBlock == null) {
 			return;
 		}
+		BlockState state = mc.world.getBlockState(playerController.currentBlock);
+		boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
+		int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
+		Color fadeColor = new Color(color);
+		Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), progressAlpha);
+		Rectangle rect = event.getPosition();
 		if (playerController.getIsHittingBlock()) {
-			BlockState state = mc.world.getBlockState(playerController.currentBlock);
-			boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
-			int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
+			float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
+			progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
+			progress = MathHelper.clamp(progress, 0, 1);
+			if (progress < 0.01) {
+				progressAlpha = 0;
+			}
 			if (System.currentTimeMillis() > fadeTime) {
 				if (progressAlpha < 200) {
 					fadeTime = System.currentTimeMillis() + 10;
 					progressAlpha = progressAlpha + 5;
 				}
 			}
-			Color fadeColor = new Color(color);
-			Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), progressAlpha);
-			System.out.println(progressAlpha);
-			Rectangle rect = event.getPosition();
-			float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
-			progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
-			progress = MathHelper.clamp(progress, 0, 1);
 			AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * progress), rect.y + rect.height + 1, alphaColor.getRGB());
 			savedProgress = progress;
-		} else {
-			if (progressAlpha > 0) {
-				Rectangle rect = event.getPosition();
-				BlockState state = mc.world.getBlockState(playerController.currentBlock);
-				boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
-				int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
-				Color fadeColor = new Color(color);
-				Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), progressAlpha);
-				AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * savedProgress), rect.y + rect.height + 1, alphaColor.getRGB());
-				if (System.currentTimeMillis() > fadeTime) {
-					fadeTime = System.currentTimeMillis() + 10;
-					progressAlpha = progressAlpha - 5;
-				}
+		}
+		if (progressAlpha > 0) {
+			AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * savedProgress), rect.y + rect.height + 1, alphaColor.getRGB());
+			if (System.currentTimeMillis() > fadeTime) {
+				fadeTime = System.currentTimeMillis() + 10;
+				progressAlpha = progressAlpha - 5;
 			}
 		}
 	}
