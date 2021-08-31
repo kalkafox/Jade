ae8a329aeab77ff6410321ae934afdd91b885d6e
Kalka <k@kalka.io>
2021-08-30 21:04:08 -0400

    Add fade animation to progress bar

--- src/main/java/snownee/jade/client/ClientHandler.java
+++ src/main/java/snownee/jade/client/ClientHandler.java
@@ -1,6 +1,6 @@
 package snownee.jade.client;
 
-import java.awt.Rectangle;
+import java.awt.*;
 
 import mcp.mobius.waila.api.RenderContext;
 import mcp.mobius.waila.api.event.WailaRenderEvent;
@@ -20,6 +20,8 @@ import snownee.jade.JadePlugin;
 @OnlyIn(Dist.CLIENT)
 @EventBusSubscriber(Dist.CLIENT)
 public final class ClientHandler {
+	private static float savedProgress;
+	private static float progressAlpha;
 
 	@SubscribeEvent
 	public static void post(WailaRenderEvent.Post event) {
@@ -28,17 +30,29 @@ public final class ClientHandler {
 		}
 		Minecraft mc = Minecraft.getInstance();
 		PlayerController playerController = mc.playerController;
-		if (playerController == null || !playerController.getIsHittingBlock()) {
+		if (playerController == null || playerController.currentBlock == null) {
 			return;
 		}
 		BlockState state = mc.world.getBlockState(playerController.currentBlock);
 		boolean canHarvest = ForgeHooks.canHarvestBlock(state, mc.player, mc.world, playerController.currentBlock);
 		int color = canHarvest ? 0x88FFFFFF : 0x88FF4444;
+		Color fadeColor = new Color(color);
+		Color alphaColor = new Color(fadeColor.getRed(), fadeColor.getGreen(), fadeColor.getBlue(), (int) MathHelper.clamp(progressAlpha, 0, 200));
 		Rectangle rect = event.getPosition();
-		float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
-		progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
-		progress = MathHelper.clamp(progress, 0, 1);
-		AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * progress), rect.y + rect.height + 1, color);
+		progressAlpha = (progressAlpha > 200 ? 200 : progressAlpha);
+		progressAlpha = (progressAlpha < 0 ? 0 : progressAlpha);
+		if (playerController.getIsHittingBlock()) {
+			float progress = state.getPlayerRelativeBlockHardness(mc.player, mc.player.world, playerController.currentBlock);
+			progress = playerController.curBlockDamageMP + mc.getRenderPartialTicks() * progress;
+			progress = MathHelper.clamp(progress, 0, 1);
+			progressAlpha = (playerController.curBlockDamageMP < 0.01 ? (progressAlpha - (savedProgress * 100)  * mc.getTickLength()) : progressAlpha);
+			progressAlpha += (savedProgress * mc.getTickLength()) * 100;
+			AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * progress), rect.y + rect.height + 1, alphaColor.getRGB());
+			savedProgress = progress;
+		} else {
+			progressAlpha -= (savedProgress * mc.getTickLength()) * 50;
+			AbstractGui.fill(RenderContext.matrixStack, rect.x + 1, rect.y + rect.height, rect.x + 1 + (int) (rect.width * savedProgress), rect.y + rect.height + 1, alphaColor.getRGB());
+		}
 	}
 
 }
